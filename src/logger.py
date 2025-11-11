56cae44be4f67ce9e73b29e27235a117a63950da
Kavisho-o <kavisheleven011@gmail.com>
2025-11-11 20:46:08 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,20 @@
 import logging
 import os
 from datetime import datetime
+
+# Create "logs" directory inside project root
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Create a unique log file with timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO
+    level=logging.INFO,
 )
+
+print(f"🪵 Logging initialized at: {LOG_FILE_PATH}")
